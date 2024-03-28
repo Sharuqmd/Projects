cf27b42f7bd9dc834216528d0e1fc82dbce48a88
Sharuqmd <142597507+Sharuqmd@users.noreply.github.com>
2024-03-28 17:28:58 +0530

    Update terraform-for-jenkins.tf

--- terraform-for-jenkins.tf
+++ terraform-for-jenkins.tf
@@ -1,1 +1,24 @@
 
+#!/bin/bash
+
+# Add the Jenkins repository key to apt
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+
+# Add the Jenkins repository to the system's package sources
+echo "deb https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list
+
+# Update your package list to include the Jenkins repository
+sudo apt update
+
+# Install Jenkins
+sudo apt install -y jenkins
+
+# Start Jenkins service
+sudo systemctl start jenkins
+
+# Enable Jenkins to start on boot
+sudo systemctl enable jenkins
+
+# Display initial admin password
+echo "Initial admin password:"
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
